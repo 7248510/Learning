e3bf8d70bd0e111225d400b45b74b1773d3aac6d
7248510 <calebkre19@gmail.com>
2020-03-29 16:06:40 -0400

    Data program designed to get file attributes.

---
+++ Java/Data/sample.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+public class sample
+{
+	public static void main(String[] args)
+	{
+		data();
+	}
+	public static void data()
+	{
+		//Get the local date
+		//Get the MD5 Hash
+		//Get the UTC
+		//DB Connection
+		LocalDate current = LocalDate.now();
+		System.out.println(current);
+	}
+}
