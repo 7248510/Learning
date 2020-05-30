bd876704ffdbcd13f1bb74069d536715508c344a
Caleb <56181475+7248510@users.noreply.github.com>
2020-05-30 17:15:47 -0400

    Create if.cpp

---
+++ C++/if.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+int main()
+{
+	//If statements in C++
+	int x = 0;
+	if (x == 90) {
+		printf("X is equal to 90");
+	}
+	else if (x == 91) {
+		printf("X is equal to 91");
+	}
+	else 
+	{
+		x = 123;
+		//std::cout << x;
+		//printf("\n%d",x);
+	}
+	printf("\n%d",x);	
+}
